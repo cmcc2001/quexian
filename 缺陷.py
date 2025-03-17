6f18ea32bbf898027b7896896f0248ef3a90484d
cmcc2001 <2455224576@qq.com>
2025-03-17 20:34:04 +0800

    Update 缺陷.py

--- 缺陷.py
+++ 缺陷.py
@@ -172,12 +172,9 @@ method_configs = {
         "氧化物俘获电荷缺陷浓度ΔNot": {
             "formula": r"\Delta V_{ot}=\frac{q\Delta N_{ot}}{C_{ox}}",
             "inputs": [
-               
-                { "label": "输入ΔVth（单位：V）",
-                "key": "ΔVth",
-                "default": 1.0 }    
-        
-            ],
+        {"label": "输入Vth（单位：V）", "key": "Vth", "default": 1.0},
+        {"label": "输入ΔNit（单位：cm-2）", "key": "ΔNit", "default": 1e-6, }
+    ],
             "calc_function": lambda ΔVth,ΔNit: (((6.91E-10* ΔVth) / 1.6e-19)-ΔNit), 
             "table_key": "gs_table1",
             "result_col": "ΔNot"
