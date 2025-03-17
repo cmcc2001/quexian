8b3732a9a28ee5c7d8675ac8204df0e0e7965683
cmcc2001 <2455224576@qq.com>
2025-03-17 15:46:51 +0800

    Update 缺陷.py

--- 缺陷.py
+++ 缺陷.py
@@ -140,10 +140,8 @@ method_configs = {
    "SS": {
        "氧化物俘获电荷缺陷浓度ΔNot": { 
     "formula": r"""
-    \begin{aligned}
-    I_d\left(th\right)=I_{do}\left(V_d\right)e^{\left(\beta V_{th}\right)\bullet\left(\beta V_{th}\right)^{-1/2}} \\
+    I_d\left(th\right)=I_{do}\left(V_d\right)e^{\left(\beta V_{th}\right)\bullet\left(\beta V_{th}\right)^{-1/2}}//
     I_{mg}=I_{do}\left(V_d\right)e^{\left(\beta\frac{V_{th}}{2}\right)\bullet\left(\beta\frac{V_{th}}{2}\right)^{-1/2}}
-    \end{aligned}
     """,
     "inputs": [
         {"label": "输入Vth（单位：V）", "key": "Vth", "default": 1.0},
